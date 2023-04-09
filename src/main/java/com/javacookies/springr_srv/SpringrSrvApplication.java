5a65414492c0edeefe8e62fcfb62a6ca885c376c
BrickerFixer <brickerfixer@gmail.com>
2023-04-09 17:09:08 +0400

    Initial commit

---
+++ src/main/java/com/javacookies/springr_srv/SpringrSrvApplication.java
@@ -0,0 +1,13 @@
+package com.javacookies.springr_srv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrSrvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringrSrvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javacookies/springr_srv/SpringrSrvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javacookies.springr_srv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringrSrvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
